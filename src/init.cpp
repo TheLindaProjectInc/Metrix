cdfd559504fd5e4d2988b83d840ca6fe273036e5
blackout00000 <35306385+blackout00000@users.noreply.github.com>
2018-11-06 08:16:26 -0500

    Update init.cpp
    3c77714 Make -proxy set all network types, avoiding a connect leak. (Gregory Maxwell)

--- src/init.cpp
+++ src/init.cpp
@@ -731,11 +731,9 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
         addrProxy = CService(mapArgs["-proxy"], 9050);
         if (!addrProxy.IsValid())
             return InitError(strprintf(_("Invalid -proxy address: '%s'"), mapArgs["-proxy"]));
-
-        if (!IsLimited(NET_IPV4))
-            SetProxy(NET_IPV4, addrProxy);
-        if (!IsLimited(NET_IPV6))
-            SetProxy(NET_IPV6, addrProxy);
+	    
+        SetProxy(NET_IPV4, addrProxy);
+        SetProxy(NET_IPV6, addrProxy);
         SetNameProxy(addrProxy);
         fProxy = true;
     }
