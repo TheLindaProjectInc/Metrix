84d9bc4a8b88de6ae590edf9fd50aa1a62568742
blackout00000 <35306385+blackout00000@users.noreply.github.com>
2018-12-10 21:33:58 -0500

    Remove ACCEPTED

--- src/main.cpp
+++ src/main.cpp
@@ -2975,8 +2975,6 @@ bool ProcessBlock(CValidationState &state, CNode* pfrom, CBlock* pblock, CDiskBl
         mapOrphanBlocksByPrev.erase(hashPrev);
     }
 
-    LogPrintf("ProcessBlock: ACCEPTED\n");
-
     return true;
 }
 
