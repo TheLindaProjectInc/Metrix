3f5b9e6db8472830bf54772b36950357771a1420
blackout00000 <blackout-001@hotmail.com>
2019-01-06 23:13:44 -0500

    revert GetCredit and apply to GetAvailableCredit

--- src/wallet.h
+++ src/wallet.h
@@ -385,16 +385,12 @@ public:
     int64_t GetCredit(const CTransaction& tx) const
     {
         int64_t nCredit = 0;
-        uint256 hashTx = GetHash();
         BOOST_FOREACH(const CTxOut& txout, tx.vout)
-        {
-        if (!pwallet->IsSpent(hashTx, i))
         {
             nCredit += GetCredit(txout);
             if (!MoneyRange(nCredit))
                 throw std::runtime_error("CWallet::GetCredit() : value out of range");
         }
-    }
         return nCredit;
     }
     int64_t GetChange(const CTransaction& tx) const
@@ -748,9 +744,10 @@ public:
             return nAvailableCreditCached;
 
         int64_t nCredit = 0;
+        uint256 hashTx = GetHash();
         for (unsigned int i = 0; i < vout.size(); i++)
         {
-            if (!IsSpent(i))
+            if (!IsSpent(hashTx, i))
             {
                 const CTxOut &txout = vout[i];
                 nCredit += pwallet->GetCredit(txout);
