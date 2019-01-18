1bf02d5ef0e47963c6a539f5b41c0240395ae5eb
nibbles83 <hardke01@yahoo.co.uk>
2019-01-18 12:41:58 +0000

    Reorganise help messages for daemon and cli.

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -68,7 +68,8 @@ bool AppInit(int argc, char* argv[])
                   "  Lindad [options] help                " + _("List commands") + "\n" +
                   "  Lindad [options] help <command>      " + _("Get help for a command") + "\n";
 
-            strUsage += "\n" + HelpMessageCli(true);
+            strUsage += "\n" + HelpMessage(HMM_BITCOIND);
+            strUsage += "\n" + HelpMessageCli(false);
 
             fprintf(stdout, "%s", strUsage.c_str());
             return false;
