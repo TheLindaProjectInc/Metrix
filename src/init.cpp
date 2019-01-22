61617adf05ae96fd32aa22300b8a24def4e34d1e
nibbles83 <hardke01@yahoo.co.uk>
2019-01-22 11:24:00 +0000

    Remove duplicate InitSanityCheck() function.

--- src/init.cpp
+++ src/init.cpp
@@ -450,23 +450,6 @@ void ThreadImport(std::vector<boost::filesystem::path> vImportFiles) {
     }
 }
 
-/** Sanity checks
- *  Ensure that Linda is running in a usable environment with all
- *  necessary library support.
- */
-bool InitSanityCheck(void)
-{
-    if (!ECC_InitSanityCheck()) {
-        InitError("OpenSSL appears to lack support for elliptic curve cryptography. For more "
-            "information, visit https://en.bitcoin.it/wiki/OpenSSL_and_EC_Libraries");
-        return false;
-    }
-
-    // TODO: remaining sanity checks, see #4081
-
-    return true;
-}
-
 /** Initialize Lindacoin.
  *  @pre Parameters should be parsed and config file should be read.
  */
