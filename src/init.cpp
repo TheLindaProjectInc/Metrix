acfcfd1588d3147336eed70a1b7bd5b4308114b9
David Stoneham <dstoneham.93@gmail.com>
2019-01-30 18:36:40 +1000

    Log aborted block database rebuilds

--- src/init.cpp
+++ src/init.cpp
@@ -1003,6 +1003,7 @@ bool AppInit2(boost::thread_group& threadGroup)
                     fReindex = true;
                     fRequestShutdown = false;
                 } else {
+                    LogPrintf("Aborted block database rebuild. Exiting.\n");
                     return false;
                 }
             } else {
