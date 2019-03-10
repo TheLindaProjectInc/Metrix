f63181b93687c571cc4d7755022eeef19f03e85a
blackout00000 <blackout-001@hotmail.com>
2019-03-09 19:51:17 -0500

    Use script matching rather than destination matching for watch-only.

--- src/walletdb.h
+++ src/walletdb.h
@@ -124,7 +124,7 @@ public:
 
     bool WriteCScript(const uint160& hash, const CScript& redeemScript);
 
-    bool WriteWatchOnly(const CTxDestination &dest);
+    bool WriteWatchOnly(const CScript &script);
 
     bool WriteBestBlock(const CBlockLocator& locator);
     bool ReadBestBlock(CBlockLocator& locator);
