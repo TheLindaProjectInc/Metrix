1a990d63fe6df2dcceda9dc1626b9dbdb66046e9
nibbles83 <hardke01@yahoo.co.uk>
2019-03-21 15:13:05 +0000

    Add "chain" to getmininginfo, improve help in getblockchaininfo

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -107,6 +107,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("netstakeweight", GetPoSKernelPS()));
     obj.push_back(Pair("errors",        GetWarnings("statusbar")));
     obj.push_back(Pair("pooledtx",      (uint64_t)mempool.size()));
+    obj.push_back(Pair("chain", Params().NetworkIDString()));
 
     weight.push_back(Pair("minimum",    (uint64_t)nWeight));
     weight.push_back(Pair("maximum",    (uint64_t)0));
