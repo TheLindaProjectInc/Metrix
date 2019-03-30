68f968d113f2d9cfa009ee2e4a59e862aa2f5481
blackout00000 <blackout-001@hotmail.com>
2019-03-29 20:50:31 -0400

    add license header to core_io.h, core_read/_write.cpp

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php
+
 #include "core.h"
 #include "serialize.h"
 #include "script.h"
