4a850950b26c10156a2663b2c53b12b0d1fc1278
nibbles83 <hardke01@yahoo.co.uk>
2019-04-01 09:04:18 +0100

    Switch miner.cpp to use sha2 instead of OpenSSL.

--- src/miner.h
+++ src/miner.h
@@ -17,16 +17,10 @@ CBlockTemplate* CreateNewBlockWithKey(CReserveKey& reservekey, CWallet* pwallet,
 /** Modify the extranonce in a block */
 void IncrementExtraNonce(CBlock* pblock, CBlockIndex* pindexPrev, unsigned int& nExtraNonce);
 
-/** Do mining precalculation */
-void FormatHashBuffers(CBlock* pblock, char* pmidstate, char* pdata, char* phash1);
-
 /** Check mined proof-of-work block */
 bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey);
 
 /** Check mined proof-of-stake block */
 bool CheckStake(CBlock* pblock, CWallet& wallet);
-
-/** Base sha256 mining transform */
-void SHA256Transform(void* pstate, void* pinput, const void* pinit);
 
 #endif // NOVACOIN_MINER_H
