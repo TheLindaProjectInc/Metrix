e4fc3136bf4af0ac2406a6fcba199cd5eaaf571e
nibbles83 <hardke01@yahoo.co.uk>
2019-04-30 11:34:29 +0100

    Broken addresses on command line no longer trigger testnet.
    When passing a bitcoin: URI on the command line, invalid addresses do not incorrectly send the
    user to the test network.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -163,26 +163,30 @@ public:
 static CTestNetParams testNetParams;
 
 
-static CChainParams *pCurrentParams = &mainParams;
+static CChainParams *pCurrentParams = 0;
 
 const CChainParams &Params() {
     assert(pCurrentParams);
     return *pCurrentParams;
 }
 
-void SelectParams(CBaseChainParams::Network network) {
-    SelectBaseParams(network);
+CChainParams& Params(CBaseChainParams::Network network)
+{
     switch (network) {
         case CBaseChainParams::MAIN:
-            pCurrentParams = &mainParams;
-            break;
+            return mainParams;
         case CBaseChainParams::TESTNET:
-            pCurrentParams = &testNetParams;
-            break;
+            return testNetParams;
         default:
             assert(false && "Unimplemented network");
-            return;
+            return mainParams;
     }
+}
+
+void SelectParams(CBaseChainParams::Network network)
+{
+    SelectBaseParams(network);
+    pCurrentParams = &Params(network);
 }
 
 bool SelectParamsFromCommandLine() {
