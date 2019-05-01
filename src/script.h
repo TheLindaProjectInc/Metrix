b07a51537b2c82e14595cf04320eee8a438448ad
nibbles83 <hardke01@yahoo.co.uk>
2019-05-01 11:34:46 +0100

    Remove unused function StackString()

--- src/script.h
+++ src/script.h
@@ -505,21 +505,6 @@ inline std::string ValueString(const std::vector<unsigned char>& vch)
         return HexStr(vch);
 }
 
-inline std::string StackString(const std::vector<std::vector<unsigned char> >& vStack)
-{
-    std::string str;
-    BOOST_FOREACH(const std::vector<unsigned char>& vch, vStack)
-    {
-        if (!str.empty())
-            str += " ";
-        str += ValueString(vch);
-    }
-    return str;
-}
-
-
-
-
 /** Serialized script, used inside transaction inputs and outputs */
 class CScript : public std::vector<unsigned char>
 {
