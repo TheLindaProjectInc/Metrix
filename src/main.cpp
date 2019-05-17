b8943368301dd08df0396ef4eb08374904b8cf82
nibbles83 <hardke01@yahoo.co.uk>
2019-05-17 15:09:55 +0100

    Move CBlockIndex, CChain and related code out of main

--- src/main.cpp
+++ src/main.cpp
@@ -311,53 +311,6 @@ bool GetNodeStateStats(NodeId nodeid, CNodeStateStats& stats)
     return true;
 }
 
-//////////////////////////////////////////////////////////////////////////////
-//
-// CChain implementation
-//
-
-CBlockIndex* CChain::SetTip(CBlockIndex* pindex)
-{
-    if (pindex == NULL) {
-        vChain.clear();
-        return NULL;
-    }
-    vChain.resize(pindex->nHeight + 1);
-    while (pindex && vChain[pindex->nHeight] != pindex) {
-        vChain[pindex->nHeight] = pindex;
-        pindex = pindex->pprev;
-    }
-    return pindex;
-}
-
-CBlockLocator CChain::GetLocator(const CBlockIndex* pindex) const
-{
-    int nStep = 1;
-    std::vector<uint256> vHave;
-    vHave.reserve(32);
-
-    if (!pindex)
-        pindex = Tip();
-    while (pindex) {
-        vHave.push_back(pindex->GetBlockHash());
-        // Stop when we have added the genesis block.
-        if (pindex->nHeight == 0)
-            break;
-        // Exponentially larger steps back, plus the genesis block.
-        int nHeight = std::max(pindex->nHeight - nStep, 0);
-        if (Contains(pindex)) {
-            // Use O(1) CChain index if possible.
-            pindex = (*this)[nHeight];
-        } else {
-            // Otherwise, use O(log n) skiplist.
-            pindex = pindex->GetAncestor(nHeight);
-        }
-        if (vHave.size() > 10)
-            nStep *= 2;
-    }
-    return CBlockLocator(vHave);
-}
-
 CBlockIndex* CChain::FindFork(const CBlockLocator& locator) const
 {
     // Find the first block the caller has in the main chain
@@ -370,15 +323,6 @@ CBlockIndex* CChain::FindFork(const CBlockLocator& locator) const
         }
     }
     return Genesis();
-}
-
-const CBlockIndex* CChain::FindFork(const CBlockIndex* pindex) const
-{
-    if (pindex->nHeight > Height())
-        pindex = pindex->GetAncestor(Height());
-    while (pindex && !Contains(pindex))
-        pindex = pindex->pprev;
-    return pindex;
 }
 
 // Requires cs_main.
