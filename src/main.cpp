1528c1b3b3cdf3c5739c0fceebab3d8731c2df7b
blackout00000 <blackout-001@hotmail.com>
2019-05-20 12:03:09 -0400

    Trigger -alertnotify if network is upgrading without you

--- src/main.cpp
+++ src/main.cpp
@@ -2134,7 +2134,9 @@ void static UpdateTip(CBlockIndex* pindexNew)
     cvBlockChange.notify_all();
 
     // Check the version of the last 100 blocks to see if we need to upgrade:
-    if (!IsInitialBlockDownload()) {
+    static bool fWarned = false;
+    if (!IsInitialBlockDownload() && !fWarned)    
+    {
         int nUpgraded = 0;
         const CBlockIndex* pindex = chainActive.Tip();
         for (int i = 0; i < 100 && pindex != NULL; i++) {
@@ -2145,8 +2147,12 @@ void static UpdateTip(CBlockIndex* pindexNew)
         if (nUpgraded > 0)
             LogPrintf("SetBestChain: %d of last 100 blocks above version %d\n", nUpgraded, (int)CBlock::CURRENT_VERSION);
         if (nUpgraded > 100 / 2)
-            // strMiscWarning is read by GetWarnings(), called by Qt and the JSON-RPC code to warn the user:
+        {
+                    // strMiscWarning is read by GetWarnings(), called by Qt and the JSON-RPC code to warn the user:
             strMiscWarning = _("Warning: This version is obsolete, upgrade required!");
+            CAlert::Notify(strMiscWarning, true);
+            fWarned = true;
+        }
     }
 }
 
