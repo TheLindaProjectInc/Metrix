45d874867da6c9883d4c647c07ce9a7e63ba1279
blackout00000 <blackout-001@hotmail.com>
2019-06-08 21:02:39 -0400

    Reduce linda-cli dependencies

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -4,7 +4,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "chainparamsbase.h"
-#include "init.h"
 #include "rpcclient.h"
 #include "rpcprotocol.h"
 #include "util.h"
