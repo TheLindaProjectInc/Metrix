a814d51fc236874fc0bf8f71d8d89e5c135b9647
nibbles83 <hardke01@yahoo.co.uk>
2019-07-12 09:43:18 +0100

    Update comments in client version to be doxygen compatible

--- src/clientversion.h
+++ src/clientversion.h
@@ -8,27 +8,27 @@
 #if defined(HAVE_CONFIG_H)
 #include "config/bitcoin-config.h"
 #else
-//
-// client versioning
-//
+/**
+* client versioning
+*/
 
-// These need to be macros, as version.cpp's and bitcoin*-res.rc's voodoo requires it
+/** These need to be macros, as version.cpp's and bitcoin*-res.rc's voodoo requires it */
 #define CLIENT_VERSION_MAJOR       3
 #define CLIENT_VERSION_MINOR       4
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
-// Set to true for release, false for prerelease or test build
+/** Set to true for release, false for prerelease or test build */
 #define CLIENT_VERSION_IS_RELEASE  false
 
-// Copyright year (2018-this)
-// Todo: update this when changing our copyright comments in the source
+/** Copyright year (2018-this) */
+/** Todo: update this when changing our copyright comments in the source */
 #define COPYRIGHT_YEAR 2019
 
 #endif //HAVE_CONFIG_H
 
-// Converts the parameter X to a string after macro replacement on X has been performed.
-// Don't merge these into one macro!
+/** Converts the parameter X to a string after macro replacement on X has been performed. */
+/** Don't merge these into one macro! */
 #define STRINGIZE(X) DO_STRINGIZE(X)
 #define DO_STRINGIZE(X) #X
 
