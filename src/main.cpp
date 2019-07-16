5745b5abd2d5e5374f971678e23b0020f4a4949a
nibbles83 <hardke01@yahoo.co.uk>
2019-07-16 12:53:41 +0100

    error checking updates

--- src/main.cpp
+++ src/main.cpp
@@ -1645,7 +1645,7 @@ void UpdateCoins(const CTransaction& tx, CValidationState& state, CCoinsViewCach
 bool CScriptCheck::operator()() const
 {
     const CScript& scriptSig = ptxTo->vin[nIn].scriptSig;
-    if (!VerifyScript(scriptSig, scriptPubKey, nFlags, CachingSignatureChecker(*ptxTo, nIn, cacheStore)))
+    if (!VerifyScript(scriptSig, scriptPubKey, nFlags, CachingSignatureChecker(*ptxTo, nIn, cacheStore), &error))
         return error("CScriptCheck() : %s:%d VerifyScript failed", ptxTo->GetHash().ToString(), nIn, ScriptErrorString(error));
     return true;
 }
