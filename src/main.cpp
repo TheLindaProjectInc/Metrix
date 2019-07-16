9b08a42bf76667d8e829dd2ba22ee46b15bf5473
nibbles83 <hardke01@yahoo.co.uk>
2019-07-16 13:26:45 +0100

    missing format specifier

--- src/main.cpp
+++ src/main.cpp
@@ -1646,7 +1646,7 @@ bool CScriptCheck::operator()()
 {
     const CScript& scriptSig = ptxTo->vin[nIn].scriptSig;
     if (!VerifyScript(scriptSig, scriptPubKey, nFlags, CachingSignatureChecker(*ptxTo, nIn, cacheStore), &error)) {
-        return ::error("CScriptCheck() : %s:%d VerifyScript failed", ptxTo->GetHash().ToString(), nIn, ScriptErrorString(error));
+        return ::error("CScriptCheck() : %s:%d VerifyScript failed %s", ptxTo->GetHash().ToString(), nIn, ScriptErrorString(error));
     }
     return true;
 }
