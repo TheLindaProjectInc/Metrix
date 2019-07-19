4f5f7f9e478f3b447ebbb7d1af0b8db13dc5a828
nibbles83 <hardke01@yahoo.co.uk>
2019-07-19 12:19:41 +0100

    Add CValidationInterface::BlockChecked notification

--- src/main.h
+++ src/main.h
@@ -627,6 +627,7 @@ protected:
     virtual void UpdatedTransaction(const uint256& hash) {};
     virtual void Inventory(const uint256& hash) {};
     virtual void ResendWalletTransactions(bool fForce) {};
+    virtual void BlockChecked(const CBlock&, const CValidationState&) {};
     friend void ::RegisterValidationInterface(CValidationInterface*);
     friend void ::UnregisterValidationInterface(CValidationInterface*);
     friend void ::UnregisterAllValidationInterfaces();
