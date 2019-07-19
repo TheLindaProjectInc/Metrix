653aba59a0fa11e40da8f36bc9653266987cc1b5
nibbles83 <hardke01@yahoo.co.uk>
2019-07-19 15:34:27 +0100

    add missing core_io.h to core_read.cpp includes list

--- src/core_read.cpp
+++ src/core_read.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php
 
+#include "core_io.h"
+
 #include "primitives/transaction.h"
 #include "script/script.h"
 #include "serialize.h"
