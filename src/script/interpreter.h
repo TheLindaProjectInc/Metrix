baecfe3152cc39f123d9b72d8288a918262e6b35
nibbles83 <hardke01@yahoo.co.uk>
2019-07-19 16:56:17 +0100

    add IsValidSignatureEncoding() to interpreter.h to ensure we can access this from main.cpp

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -113,6 +113,7 @@ public:
     MutableTransactionSignatureChecker(const CMutableTransaction* txToIn, unsigned int nInIn) : TransactionSignatureChecker(&txTo, nInIn), txTo(*txToIn) {}
 };
 
+bool static IsValidSignatureEncoding(const std::vector<unsigned char> &sig);
 bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* error = NULL);
 bool VerifyScript(const CScript& scriptSig, const CScript& scriptPubKey, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* error = NULL);
 
