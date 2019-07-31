79be0716e3c107f428d841a739b30044f60a9c09
David Stoneham <dstoneham.93@gmail.com>
2019-07-31 18:09:31 +1000

    fix setaccount creating new addresses

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -270,13 +270,6 @@ Value setaccount(const Array& params, bool fHelp)
     //! Only add the account if the address is yours.
     if (IsMine(*pwalletMain, address.Get()))
     {
-        //! Detect when changing the account of an address that is the 'unused current key' of another account:
-        if (pwalletMain->mapAddressBook.count(address.Get()))
-        {
-            string strOldAccount = pwalletMain->mapAddressBook[address.Get()].name;
-            if (address == GetAccountAddress(strOldAccount))
-                GetAccountAddress(strOldAccount, true);
-        }
         pwalletMain->SetAddressBook(address.Get(), strAccount, "receive");
     }
     else
