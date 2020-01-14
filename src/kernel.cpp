5d3142673d83a54958acbaec8e01871a1243eb07
nibbles83 <hardke01@yahoo.co.uk>
2020-01-14 11:18:02 +0000

    Remove references to X11 licence

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2012-2013 The PPCoin developers
 // Copyright (c) 2014 The Metrix developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <boost/assign/list_of.hpp>
--- src/miner.h
+++ src/miner.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2013 The NovaCoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef NOVACOIN_MINER_H
 #define NOVACOIN_MINER_H
