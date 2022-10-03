15c70e948e191ecbe84fb2029e8cae762171a308
Jon <squid@sqdmc.net>
2022-10-02 21:59:59 -0400

    update contracts for regtest

--- src/qtum/qtumDGP.h
+++ src/qtum/qtumDGP.h
@@ -8,9 +8,9 @@
 
 // DGP Version 2 [MIP3] Contract Addresses
 // TODO: deploy mainnet; remove regtest
-static const dev::Address DGPContract_v2 = dev::Address("0xe031e39e74c915978bd031dd77d9c864207b6acb");
-static const dev::Address GovernanceDGP_v2 = dev::Address("0x90e71d16b3f49b957dea91e830f70187b0610501");
-static const dev::Address BudgetDGP_v2 = dev::Address("0x7c71de9a32539d3819ddb444c2ccc54b758068cc");
+static const dev::Address DGPContract_v2 = dev::Address("0x037ffe1173cca78c4e5c8a3d1eb1001b615f75be");
+static const dev::Address GovernanceDGP_v2 = dev::Address("0xe30458ac7711a738e21622efefbc93aa2d31cc7a");
+static const dev::Address BudgetDGP_v2 = dev::Address("0x0ba5b180277783a5889fa6ee0aa24fbf59afe3d7");
 // DGP Version 1 [original] Contract Addresses
 static const dev::Address DGPContract_v1 = dev::Address("0x0000000000000000000000000000000000000088");
 static const dev::Address GovernanceDGP_v1 = dev::Address("0x0000000000000000000000000000000000000089");
