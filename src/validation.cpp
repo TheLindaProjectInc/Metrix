f8e5fcba3d155455dcd108a8b00522f92dfef726
Jon <squid@sqdmc.net>
2023-06-12 04:22:51 -0400

    okay.. enum fix

--- src/validation.cpp
+++ src/validation.cpp
@@ -4254,7 +4254,7 @@ void static UpdateTip(const CBlockIndex* pindexNew, const CChainParams& chainPar
         int nUpgraded = 0;
         const CBlockIndex* pindex = pindexNew;
         for (int bit = 0; bit < VERSIONBITS_NUM_BITS; bit++) {
-            for (int id = 0; id < DeploymentPos::MAX_VERSION_BITS_DEPLOYMENTS; id++) {
+            for (int id = 0; id < Consensus::DeploymentPos::MAX_VERSION_BITS_DEPLOYMENTS; id++) {
                 WarningBitsConditionChecker checker(bit, (Consensus::DeploymentPos) id);
                 ThresholdState state = checker.GetStateFor(pindex, chainParams.GetConsensus(), warningcache[bit]);
                 if (state == ThresholdState::ACTIVE || state == ThresholdState::LOCKED_IN) {
